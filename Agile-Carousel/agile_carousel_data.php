d6fd52fc5383b06e2bfdcd0b0e9ecaf9de8ea6a7
Peter Watters <mcudar@gmail.com>
2014-07-26 18:51:22 +0100

    Initial commit

---
+++ Agile-Carousel/agile_carousel_data.php
@@ -0,0 +1,19 @@
+[{
+      "content": "<div class='slide_inner'><div class='slide_inner_image'><a class='photo_link' href='#'><img class='photo' src='images/carousel/integra.jpg' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div><div class='slide_inner_text'>This is some text</div>  </div>",
+      "content_button": "<div class='thumb'><img src='images/carousel/integra_thumb.jpg' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+}, {
+      "content": "<div class='slide_inner'><div class='slide_inner_image'><a class='photo_link' href='#'><img class='photo' src='images/carousel/ocuco.jpg' alt='Paint'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div><div class='slide_inner_text'></div></div>",
+      "content_button": "<div class='thumb'><img src='images/carousel/ocuco_thumb.jpg' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+}, {
+      "content": "<div class='slide_inner'><div class='slide_inner_image'><a class='photo_link' href='#'><img class='photo' src='images/carousel/fitnessperformancesystems.jpg' alt='Tunnel'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div><div class='slide_inner_text'></div></div>",
+      "content_button": "<div class='thumb'><img src='images/carousel/fitnessperformancesystems_thumb.jpg' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+}, {
+      "content": "<div class='slide_inner'><div class='slide_inner_image'><a class='photo_link' href='#'><img class='photo' src='images/carousel/peterjwatters_OLD_home.jpg' alt='Paint'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div><div class='slide_inner_text'></div></div>",
+      "content_button": "<div class='thumb'><img src='images/carousel/peterjwatters_OLD_home_thumb.jpg' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+}, {
+      "content": "<div class='slide_inner'><div class='slide_inner_image'><a class='photo_link' href='#'><img class='photo' src='images/carousel/tnf_dashboard_home.jpg' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div><div class='slide_inner_text'></div></div>",
+      "content_button": "<div class='thumb'><img src='images/carousel/tnf_dashboard_home_thumb.jpg' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+}, {
+      "content": "<div class='slide_inner'><div class='slide_inner_image'><a class='photo_link' href='#'><img class='photo' src='images/carousel/tnf_inventory_home.jpg' alt='Paint'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div><div class='slide_inner_text'></div></div>",
+      "content_button": "<div class='thumb'><img src='images/carousel/tnf_inventory_home_thumb.jpg' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+}]
